3734bdc5ad5a7dad35246f37ec642f27f6e5a72e
Qobil7337 <qobil7337@mail.ru>
2024-03-20 19:44:37 +0500

    ready to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './section/main/main.component';
+import { AboutUsComponent } from './sections/about-us/about-us.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { OurTeamComponent } from './sections/our-team/our-team.component';
+import { CompanyInNumbersComponent } from './sections/company-in-numbers/company-in-numbers.component';
+import { CompanyExperienceComponent } from './sections/company-experience/company-experience.component';
+import { OurClientsComponent } from './sections/our-clients/our-clients.component';
+import { MapComponent } from './sections/map/map.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    OurTeamComponent,
+    CompanyInNumbersComponent,
+    CompanyExperienceComponent,
+    OurClientsComponent,
+    MapComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sections/company-in-numbers/company-in-numbers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-in-numbers',
+  templateUrl: './company-in-numbers.component.html',
+  styleUrl: './company-in-numbers.component.css'
+})
+export class CompanyInNumbersComponent {
+
+}
